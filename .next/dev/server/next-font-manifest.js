7f4eb27020031e19ca75e556f93a22b449c19928
woojuman <ppagu9@naver.com>
2026-04-18 13:11:20 +0900

    커밋인가

--- .next/dev/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/(public)/page\": [\n      \"static/media/fca78c2ee6cec166-s.p.0kizb7t94.kuf.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
--- .next/dev/static/chunks/app_(public)_page_tsx_0yh6k7a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_0slx6_o._.js","static/chunks/components_home_ImageSlider_tsx_0ag2~ot._.js"],
-    source: "dynamic"
-});
